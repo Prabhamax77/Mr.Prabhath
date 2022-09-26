5de847a64a335b0f25eae6a63965b299b05fb04f
Prabhamax77 <114403254+Prabhamax77@users.noreply.github.com>
2022-09-26 17:35:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
+global.vcardowner = ['94769954216'] //ur owner number
 global.ownername = "Mr.Prabhath" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
@@ -45,8 +45,8 @@ global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 //BOT COSTOMIZEDTATION
 global.owner = ['94711421243']
 global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.botnumber = '94769954216' //Add your Bot Number For Log
+global.botname = 'Mr.Prabhath' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
